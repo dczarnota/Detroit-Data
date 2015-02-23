f03db4ffe1abdd1e3f5da5bdf7f7a6f1186b97cc
Dominic Czarnota <dczarno@gmail.com>
2015-02-22 18:59:50 -0800

    (feat) adds angular ui router, routes, and scripts file

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-angular.module('app', ['ui.bootstrap']);
+angular.module('app', ['ui.router', 'ui.bootstrap']);
+
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+
+    //Routes to homepage on default
+    $urlRouterProvider
+      .otherwise('/');
+
+    $stateProvider
+
+      .state('main', {
+        url: '/',
+        templateUrl: 'scripts/main.html',
+        controller: 'MainController'
+      });
+
+}]);
